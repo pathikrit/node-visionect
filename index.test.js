b1d39939595ef1309821adafacf869a03ebb94a0
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2023-03-06 00:03:28 -0500

    fix test names

--- index.test.js
+++ index.test.js
@@ -4,35 +4,30 @@ const {apiServer, apiKey, apiSecret, uuid} = process.env
 const VisionectApiClient = require('./index.js')
 const visionect = new VisionectApiClient(process.env)
 
-// Monkey patch the call method
-// visionect._call = visionect.call
-// visionect.call = (method, path, data) => {
-//   console.assert(method === 'GET', 'Aborting non-GET API call in tests')
-//   return {method: method, path: path, promise: visionect._call(method, path, data)}
-// }
-
 visionect.http.interceptors.request.use(req => {
-  console.assert(req.method === 'GET', 'Cannot make non-GET API calls from tests')
+  req.method = req.method.toUpperCase()
+  console.assert(req.method === 'GET', `Cannot make ${req.method} API calls from tests`)
   return req
 })
 
-check = (...apis) => apis.forEach(api => {
-  test(`${api.method} ${api.path}`, () => api.then(res => {
-    console.debug(api.path, '\n', res.headers['content-type'] === 'application/json' ? res.data : res.headers['content-type'])
+check = (...tests) => tests.forEach(t => {
+  const [name, api] = t
+  test(name, () => api.then(res => {
+    console.debug(res.config.method, res.config.url, '\n', res.headers['content-type'] === 'application/json' ? res.data : res.headers['content-type'])
     expect(res.status).toBe(200)
   }))
 })
 
 check(
-  visionect.devices.get(),
-  visionect.devices.get(uuid),
-  visionect.devices.config(uuid),
-  visionect.sessions.get(),
-  visionect.sessions.get(uuid),
-  visionect.users.get(),
-  visionect.users.get(apiKey),
-  visionect.config(),
-  visionect.status(),
-  visionect.devices.view(uuid).get(),
-  visionect.orphans(),
+  ['Get all devices', visionect.devices.get()],
+  ['Get one device', visionect.devices.get(uuid)],
+  ['Get device config', visionect.devices.config(uuid)],
+  ['Get all sessions', visionect.sessions.get()],
+  ['Get one session', visionect.sessions.get(uuid)],
+  ['Get all users', visionect.users.get()],
+  ['Get one user', visionect.users.get(apiKey)],
+  ['Get server config', visionect.config()],
+  ['Get server status', visionect.status()],
+  ['Get live view', visionect.devices.view(uuid).get()],
+  ['Get server orphans', visionect.orphans()],
 )
