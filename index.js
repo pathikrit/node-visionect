a5876efa80d0e3d6ca8cba961b19211865472811
pathikrit <pathikritbhowmick@msn.com>
2023-03-10 11:04:06 -0500

    update docs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto')
 const axios = require('axios')
 const _ = require('lodash')
-const curlirize = require('axios-curlirize')
 
 class VisionectApiClient {
   constructor({apiServer, apiKey, apiSecret}) {
--- index.test.js
+++ index.test.js
@@ -3,28 +3,28 @@ const {apiServer, apiKey, apiSecret, uuid} = process.env
 const curlirize = require('axios-curlirize')
 
 const VisionectApiClient = require('./index.js')
-const visionect = new VisionectApiClient(process.env)
-curlirize(visionect.http)
+const vss = new VisionectApiClient(process.env)
+curlirize(vss.http)
 
-visionect.http.interceptors.request.use(req => {
-  console.assert(req.method === 'GET', `Cannot make ${req.method} API calls from tests`)
-  return req
+vss.http.interceptors.request.use(req => {
+  req.method = req.method.toUpperCase()
+  return req.method === 'GET' ? req : Promise.reject(`Cannot make ${req.method} API calls from tests`)
 })
 
 test.each([
-  visionect.devices.get(),
-  visionect.devices.get(uuid),
-  visionect.devices.config(uuid),
-  visionect.devices.get(uuid, Date.now()/1000 - 60*60),
-  visionect.view.device(uuid),
-  visionect.view.server(uuid),
-  visionect.sessions.get(),
-  visionect.sessions.get(uuid),
-  visionect.users.get(),
-  visionect.users.get(apiKey),
-  visionect.server.config(),
-  visionect.server.status(),
-  visionect.server.orphans(),
+  vss.devices.get(),
+  vss.devices.get(uuid),
+  vss.devices.config(uuid),
+  vss.devices.get(uuid, Date.now()/1000 - 60*60),
+  vss.view.device(uuid),
+  vss.view.server(uuid),
+  vss.sessions.get(),
+  vss.sessions.get(uuid),
+  vss.users.get(),
+  vss.users.get(apiKey),
+  vss.server.config(),
+  vss.server.status(),
+  vss.server.orphans(),
 ])('API call %#', f => f.then(res => {
   console.debug(res.request.method, res.request.path, '\n', res.headers['content-type'] === 'application/json' ? res.data : res.headers['content-type'])
   expect(res.status).toBe(200)
